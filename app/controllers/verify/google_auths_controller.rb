3c375d867c9d91f69d9d844063f5229c6bb9e0f7
Somyaweb <er.bhdhiprakash@gmail.com>
2018-12-23 13:45:56 +0530

    update code

--- app/controllers/verify/google_auths_controller.rb
+++ app/controllers/verify/google_auths_controller.rb
@@ -18,7 +18,9 @@ module Verify
         @google_auth.active! and unlock_two_factor!
         redirect_to settings_path, notice: t('.notice')
       else
-        redirect_to verify_google_auth_path, alert: t('.alert')
+        flash[:alert] = "Password for authentication is incorrect, please re-enter. n1"
+        redirect_to verify_google_auth_path
+        #, alert: t('.alert')
       end
     end
 
